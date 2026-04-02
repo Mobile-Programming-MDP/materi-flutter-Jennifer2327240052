37c506793e05e0b86607b2882476461b7d280eed
Jennifer2327240052 <jennifer_2327240052@mhs.mdp.ac.id>
2026-04-02 13:09:11 +0700

    update

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pertemuan10/screens/sign_in_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? _idToken = " ";
+  String? _uid = " ";
+  String? _email = " ";
+
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+      (route) => false,
+    );
+  }
+
+  Future<void> getFirebaseAuthUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      _uid = user.uid;
+      _email = user.email;
+      String? token = await user.getIdToken(true);
+      setState(() {
+        _idToken = token;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Cepu App Home')),
-      body: const Center(child: Text('Welcome to the Home Screen!')),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("You have been signed in with Token id: $_idToken"),
+            Text("Current User: $_uid"),
+            Text("Your email: $_email"),
+          ],
+        ),
+      ),
     );
   }
 }
--- cepu_app/lib/screens/sign_in_screen.dart
+++ cepu_app/lib/screens/sign_in_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pertemuan10/screens/home_screen.dart';
+import 'package:pertemuan10/screens/sign_up_screen.dart';
+import 'package:pertemuan10/screens/home_screen.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
 
   @override
@@ -8,23 +12,82 @@ class SignInScreen extends StatelessWidget {
 }
 
 class SignInScreenState extends State<SignInScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  String _errorMessage = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Sign In')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 16.0),
-            TextField(decoration: const InputDecoration(labelText: 'Email')),
-            const SizedBox(height: 16.0),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 32.0),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                    );
+                  } catch (error) {
+                    setState(() {
+                      _errorMessage = error.toString();
+                    });
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text(_errorMessage)));
+                  }
+                },
+                child: const Text('Sign In'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle Google Sign In
+                },
+                child: Text('Sign In with Google'),
+              ),
+              const SizedBox(height: 32.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignUpScreen(),
+                    ),
+                  );
+                },
+                child: const Text('Don\'t have an account? Sign up'),
+              ),
+            ],
+          ),
         ),
       ),
     );
